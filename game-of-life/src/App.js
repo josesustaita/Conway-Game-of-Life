e84c72e64bdd9ef6738b41c5f8f138b7636c4d28
Jose Sustaita <josesustaita70@gmail.com>
2020-09-27 18:14:12 -0400

    Conway's game of Life MVP

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -3,19 +3,34 @@ import Game from "./GameBoard/Game";
 import Button from "./Buttons/Button";
 import "./App.css";
 
-export default function App() {
+export default function App(props) {
   return (
     <div>
       <h1 className="Header">The Game of Life!</h1>
-      <div className="Main-Board">
-        <p1 className="Generations">Generation: # {}</p1>
-        <Game />
-      </div>
-
-      <div className="Bottom-Row-Buttons">
-        <Button name="Start" />
-        <Button name="Pause" />
-        <Button name="Stop" />
+      <div className="Main-Container">
+        {" "}
+        <div className="Main-Board">
+          <Game />
+        </div>
+        <div className="Rules">
+          <h4>Rules</h4>
+          <p1>
+            1. Any live cell with fewer than two live neighbours dies, as if by
+            underpopulation.
+          </p1>
+          <p1>
+            2. Any live cell with two or three live neighbours lives on to the
+            next generation.
+          </p1>
+          <p1>
+            3. Any live cell with more than three live neighbours dies, as if by
+            overpopulation.
+          </p1>
+          <p1>
+            4. Any dead cell with exactly three live neighbours becomes a live
+            cell, as if by reproduction.
+          </p1>
+        </div>
       </div>
     </div>
   );
--- game-of-life/src/GameBoard/Game.js
+++ game-of-life/src/GameBoard/Game.js
@@ -1,10 +1,269 @@
 import React from "react";
 import "./Game.css";
 
-export default function Game() {
-  return (
-    <div>
-      <div className="Board"></div>
-    </div>
-  );
+var randomColor = require("randomcolor");
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+// Renders cells to the board
+class Cell extends React.Component {
+  render() {
+    const { x, y } = this.props;
+    return (
+      <div
+        className="Cell"
+        // Style of cells
+        style={{
+          left: `${CELL_SIZE * x + 1}px`,
+          top: `${CELL_SIZE * y + 1}px`,
+          width: `${CELL_SIZE - 1}px`,
+          height: `${CELL_SIZE - 1}px`,
+          background: randomColor(),
+        }}
+      />
+    );
+  }
 }
+
+// Board
+
+class Game extends React.Component {
+  constructor() {
+    super();
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
+
+    this.board = this.makeEmptyBoard();
+  }
+
+  // State of Board
+
+  state = {
+    cells: [],
+    isRunning: false,
+    interval: 100,
+    generations: 0,
+  };
+
+  makeEmptyBoard() {
+    let board = [];
+    for (let y = 0; y < this.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++) {
+        board[y][x] = false;
+      }
+    }
+
+    return board;
+  }
+
+  // Method to calculate position of the board element
+
+  getElementOffset() {
+    const rect = this.boardRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: rect.left + window.pageXOffset - doc.clientLeft,
+      y: rect.top + window.pageYOffset - doc.clientTop,
+    };
+  }
+
+  // Create cells for the board
+
+  makeCells() {
+    let cells = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[y][x]) {
+          cells.push({ x, y });
+        }
+      }
+    }
+
+    return cells;
+  }
+
+  // Event handler used to retrieve the click position, then convert it to relative position, and calculate the cols and rows of the cell being clicked
+
+  handleClick = (event) => {
+    const elemOffset = this.getElementOffset();
+    const offsetX = event.clientX - elemOffset.x;
+    const offsetY = event.clientY - elemOffset.y;
+
+    const x = Math.floor(offsetX / CELL_SIZE);
+    const y = Math.floor(offsetY / CELL_SIZE);
+
+    if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+      this.board[y][x] = !this.board[y][x];
+    }
+
+    this.setState({ cells: this.makeCells() });
+  };
+
+  runGame = () => {
+    this.setState({ isRunning: true });
+    this.runIteration();
+  };
+
+  stopGame = () => {
+    this.setState({ isRunning: false });
+    if (this.timeoutHandler) {
+      // Allows iterations to stop by calling clearTimeout
+      window.clearTimeout(this.timeoutHandler);
+      this.timeoutHandler = null;
+    }
+  };
+
+  // Method to update board state with every interation
+
+  // 1. Any live cell with fewer than two live neighbors dies, as if caused by under population.
+  // 2. Any live cell with two or three live neighbors lives on to the next generation.
+  // 3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+  // 4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+  runIteration() {
+    let newBoard = this.makeEmptyBoard();
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let neighbors = this.calculateNeighbors(this.board, x, y);
+        if (this.board[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newBoard[y][x] = true;
+          } else {
+            newBoard[y][x] = false;
+          }
+        } else {
+          if (!this.board[y][x] && neighbors === 3) {
+            newBoard[y][x] = true;
+          }
+        }
+      }
+    }
+
+    // Keeps track of generations made
+    this.setState({
+      generations: this.state.generations + 1,
+    });
+
+    this.board = newBoard;
+    this.setState({ cells: this.makeCells() });
+    this.timeoutHandler = window.setTimeout(() => {
+      this.runIteration();
+    }, this.state.interval);
+  }
+
+  /**
+   * Calculate the number of neighbors at point (x, y)
+   * @param {Array} board
+   * @param {int} x
+   * @param {int} y
+   */
+  calculateNeighbors(board, x, y) {
+    let neighbors = 0;
+    const dirs = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1],
+    ];
+    for (let i = 0; i < dirs.length; i++) {
+      const dir = dirs[i];
+      let y1 = y + dir[0];
+      let x1 = x + dir[1];
+
+      if (
+        x1 >= 0 &&
+        x1 < this.cols &&
+        y1 >= 0 &&
+        y1 < this.rows &&
+        board[y1][x1]
+      ) {
+        neighbors++;
+      }
+    }
+
+    return neighbors;
+  }
+
+  handleIntervalChange = (event) => {
+    this.setState({ interval: event.target.value });
+  };
+
+  handleClear = () => {
+    this.board = this.makeEmptyBoard();
+    this.setState({ cells: this.makeCells() });
+    this.setState({ generations: 0 });
+  };
+
+  handleRandom = () => {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        this.board[y][x] = Math.random() >= 0.5;
+      }
+    }
+
+    this.setState({ cells: this.makeCells() });
+  };
+
+  render() {
+    const { cells, interval, isRunning } = this.state;
+    return (
+      <div>
+        <p className="Generations">Generations# {this.state.generations}</p>
+        <div
+          // Creates the grid on the board
+
+          className="Board"
+          style={{
+            width: WIDTH,
+            height: HEIGHT,
+            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
+          }}
+          // On click event to show cell when it is clicked
+
+          onClick={this.handleClick}
+          ref={(n) => {
+            this.boardRef = n;
+          }}
+        >
+          {cells.map((cell) => (
+            <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`} />
+          ))}
+        </div>
+        <div className="controls">
+          Update every{" "}
+          <input
+            // Controls for game
+
+            value={this.state.interval}
+            onChange={this.handleIntervalChange}
+          />{" "}
+          msec
+          {isRunning ? (
+            <button className="button" onClick={this.stopGame}>
+              Stop
+            </button>
+          ) : (
+            <button className="button" onClick={this.runGame}>
+              Run
+            </button>
+          )}
+          <button className="button" onClick={this.handleRandom}>
+            Random
+          </button>
+          <button className="button" onClick={this.handleClear}>
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
