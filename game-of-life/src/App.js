169949f4a6bce2529279c244391203f87f5a97c2
Jose Sustaita <josesustaita70@gmail.com>
2020-09-28 16:59:05 -0400

    Fixed placement of rules

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -13,22 +13,24 @@ export default function App(props) {
         </div>
         <div className="Rules">
           <h4>Rules</h4>
-          <p1>
-            1. Any live cell with fewer than two live neighbours dies, as if by
-            underpopulation.
-          </p1>
-          <p1>
-            2. Any live cell with two or three live neighbours lives on to the
-            next generation.
-          </p1>
-          <p1>
-            3. Any live cell with more than three live neighbours dies, as if by
-            overpopulation.
-          </p1>
-          <p1>
-            4. Any dead cell with exactly three live neighbours becomes a live
-            cell, as if by reproduction.
-          </p1>
+          <div className="Numbers">
+            <p1>
+              1. Any live cell with fewer than two live neighbours dies, as if
+              by underpopulation.
+            </p1>
+            <p1>
+              2. Any live cell with two or three live neighbours lives on to the
+              next generation.
+            </p1>
+            <p1>
+              3. Any live cell with more than three live neighbours dies, as if
+              by overpopulation.
+            </p1>
+            <p1>
+              4. Any dead cell with exactly three live neighbours becomes a live
+              cell, as if by reproduction.
+            </p1>
+          </div>
         </div>
       </div>
     </div>
